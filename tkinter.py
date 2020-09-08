6cb6fb3910681930bf7e1a57136bba2a41f32209
graff777 <61539827+graff777@users.noreply.github.com>
2020-09-08 20:10:20 +0500

    Add files via upload

---
+++ tkinter.py
@@ -0,0 +1,18 @@
+from tkinter import *
+root = Tk()
+Button(root, text = '+').grid(row = 1, column = 1)
+Button(root, text = '-').grid(row = 1, column = 2)
+Button(root, text = '0').grid(row = 1, column = 3)
+Button(root, text = '1').grid(row = 2, column = 1)
+Button(root, text = '2').grid(row = 2, column = 2)
+Button(root, text = '3').grid(row = 2, column = 3)
+Button(root, text = '4').grid(row = 3, column = 1)
+Button(root, text = '5').grid(row = 3, column = 2)
+Button(root, text = '6').grid(row = 3, column = 3)
+Button(root, text = '7').grid(row = 4, column = 1)
+Button(root, text = '8').grid(row = 4, column = 2)
+Button(root, text = '9').grid(row = 4, column = 3)
+root.mainloop()
+
+
+
