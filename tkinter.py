eecdb21bbdd1ce263b05789bcf960bf5f86d61e7
graff777 <61539827+graff777@users.noreply.github.com>
2020-09-11 13:04:10 +0500

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,18 +1,65 @@
 from tkinter import *
-root = Tk()
-Button(root, text = '+').grid(row = 1, column = 1)
-Button(root, text = '-').grid(row = 1, column = 2)
-Button(root, text = '0').grid(row = 1, column = 3)
-Button(root, text = '1').grid(row = 2, column = 1)
-Button(root, text = '2').grid(row = 2, column = 2)
-Button(root, text = '3').grid(row = 2, column = 3)
-Button(root, text = '4').grid(row = 3, column = 1)
-Button(root, text = '5').grid(row = 3, column = 2)
-Button(root, text = '6').grid(row = 3, column = 3)
-Button(root, text = '7').grid(row = 4, column = 1)
-Button(root, text = '8').grid(row = 4, column = 2)
-Button(root, text = '9').grid(row = 4, column = 3)
-root.mainloop()
 
 
+class Main(Frame):
+    def __init__(self, root):
+        super(Main, self).__init__(root)
+        self.build()
 
+    def build(self):
+        self.formula = "0"
+        self.lbl = Label(text=self.formula, font=("Times New Roman", 21, "bold"), bg="#ff0000", foreground="#FFF")
+        self.lbl.place(x=11, y=50)
+
+        btns = [
+            "C", "DEL", "*", "=",
+            "1", "2", "3", "/",
+            "4", "5", "6", "+",
+            "7", "8", "9", "-",
+            "(", "0", ")", "X^2"
+        ]
+
+        x = 10
+        y = 140
+        for bt in btns:
+            com = lambda x=bt: self.logicalc(x)
+            Button(text=bt, bg="#FFF",
+                   font=("Times New Roman", 15),
+                   command=com).place(x=x, y=y,
+                                      width=115,
+                                      height=79)
+            x += 117
+            if x > 400:
+                x = 10
+                y += 81
+
+    def logicalc(self, operation):
+        if operation == "C":
+            self.formula = ""
+        elif operation == "DEL":
+            self.formula = self.formula[0:-1]
+        elif operation == "X^2":
+            self.formula = str((eval(self.formula))**2)
+        elif operation == "=":
+            self.formula = str(eval(self.formula))
+        else:
+            if self.formula == "0":
+                self.formula = ""
+            self.formula += operation
+        self.update()
+
+    def update(self):
+        if self.formula == "":
+            self.formula = "0"
+        self.lbl.configure(text=self.formula)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root["bg"] = "#ff0000"
+    root.geometry("485x550+200+200")
+    root.title("Pentium 1")
+    root.resizable(False, False)
+    app = Main(root)
+    app.pack()
+    root.mainloop()
