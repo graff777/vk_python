64459d878abede121b3e2f5ebac0245d8496b4cf
graff777 <61539827+graff777@users.noreply.github.com>
2020-09-11 20:23:15 +0500

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,6 +1,4 @@
 from tkinter import *
-
-
 class Main(Frame):
     def __init__(self, root):
         super(Main, self).__init__(root)
@@ -9,9 +7,8 @@ class Main(Frame):
     def build(self):
         self.formula = "0"
         self.lbl = Label(text=self.formula, font=("Times New Roman", 21, "bold"), bg="#ff0000", foreground="#FFF")
-        self.lbl.place(x=11, y=50)
-
-        btns = [
+        self.lbl.place(x=11, y=10)
+        buttons = [
             "C", "DEL", "*", "=",
             "1", "2", "3", "/",
             "4", "5", "6", "+",
@@ -20,20 +17,20 @@ class Main(Frame):
         ]
 
         x = 10
-        y = 140
-        for bt in btns:
-            com = lambda x=bt: self.logicalc(x)
-            Button(text=bt, bg="#FFF",
+        y = 60
+        for button in buttons:
+            com = lambda x=button: self.logic(x)
+            Button(text=button, bg="#FFF",
                    font=("Times New Roman", 15),
                    command=com).place(x=x, y=y,
-                                      width=115,
-                                      height=79)
-            x += 117
-            if x > 400:
+                                      width=50,
+                                      height=50)
+            x += 52
+            if x > 200:
                 x = 10
-                y += 81
+                y += 52
 
-    def logicalc(self, operation):
+    def logic(self, operation):
         if operation == "C":
             self.formula = ""
         elif operation == "DEL":
@@ -53,13 +50,17 @@ class Main(Frame):
             self.formula = "0"
         self.lbl.configure(text=self.formula)
 
-
 if __name__ == '__main__':
     root = Tk()
     root["bg"] = "#ff0000"
-    root.geometry("485x550+200+200")
+    root.geometry("230x330+200+200")
     root.title("Pentium 1")
     root.resizable(False, False)
     app = Main(root)
     app.pack()
     root.mainloop()
+
+
+
+
+
